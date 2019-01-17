87344826cd38c5892fad0a4a86d178691c6aa199
Abhishek Singh <abhishek.abhi90@gmail.com>
2019-01-17 14:01:07 +0530

    More addition to multiple inheritance
    This is for demonstrating multiple inheritance in Python 3.

--- oops6.py
+++ oops6.py
@@ -2,15 +2,19 @@
 
 class OperatingSystem:
     multitasking = True
+    name = "OS"
 
 
 class Apple:
     website = "www.apple.com"
+    name = "Apple"
 
 
 class MacBook(OperatingSystem,Apple):
     def __init__(self):
-	    if self.multitasking is True:
-		    print("This is a multi-tasking system.Visit {} for more details.".format(self.website))
+        if self.multitasking is True:
+            print("This is a multi-tasking system.Visit {} for more details.".format(self.website))
+        print(self.name)
+
 
 mac1 = MacBook()
